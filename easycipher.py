793c020dc06f5ae05e615b44721d4b631172c805
Marco Catellani <marco@catellanielettronica.it>
2021-10-06 14:42:50 +0200

    cambiati gli import per funzionare con pyinstaller e quando la lib viene importata. Non funziona il file test.py se lanciato nella stessa cartella.

--- easycipher.py
+++ easycipher.py
@@ -1,31 +1,14 @@
 from typing import Union, Optional
 import base64
-
-import_mode = 'local'
-def import_local_module(module, import_file=False):
-    def try_or(func, default=None, expected_exc=(Exception,)):
-        global import_mode
-        try:
-            exec(func, globals())
-            import_mode = 'local'
-        except Exception as ex:
-            if default is not None:
-                exec(default, globals())
-                import_mode = 'path'
-    if not import_file:
-        try_or(f"from . {module} import {module}", default=f"import {module}")
-    else:
-        try_or(f"from . import {module}", default=f"import {module}")
-
-import_local_module('aes_256_cbc')
-import_local_module('openssl___aes_256_cbc')
-import_local_module('salsa20')
-import_local_module('hashfunctions', True)
+from . import aes_256_cbc
+from . import openssl___aes_256_cbc
+from . import salsa20
+from . import hashfunctions
 
 class EasyCipher:
-    VERSION = '1.3.0'
+    VERSION = '1.3.1'
     AUTHOR = 'Marco Catellani (marco@catellanielettronica.it)'
-    LAST_MODIFIED = '05/10/2021'
+    LAST_MODIFIED = '06/10/2021'
     MODIFIED_BY = 'Marco Catellani (marco@catellanielettronica.it)'
     CHANGELOG = ''''''
     DESCRIPTION = ''''''
@@ -44,18 +27,14 @@ class EasyCipher:
     def __init_crypto_obj(self):
         if self.__crypto_obj is None:
             if self.algo.lower() == 'aes_256_cbc':
-                pre = 'aes_256_cbc.' if import_mode == 'path' else ''
-                self.__crypto_obj = eval(f'{pre}aes_256_cbc(self.password)')
+                self.__crypto_obj = aes_256_cbc.aes_256_cbc(self.password)
             elif self.algo.lower() == 'openssl>=1.1.0_aes_256_cbc':
-                pre = 'openssl___aes_256_cbc.' if import_mode == 'path' else ''
-                self.__crypto_obj = eval(f'{pre}openssl___aes_256_cbc(self.password)')
+                self.__crypto_obj = openssl___aes_256_cbc.openssl___aes_256_cbc(self.password)
             elif self.algo.lower() == 'openssl<1.1.0_aes_256_cbc':
-                pre = 'openssl___aes_256_cbc.' if import_mode == 'path' else ''
-                self.__crypto_obj = eval(f'{pre}openssl___aes_256_cbc(self.password)')
+                self.__crypto_obj = openssl___aes_256_cbc.openssl___aes_256_cbc(self.password)
                 self.__crypto_obj.openssl_version_minor_of_1_1_0 = True
             elif self.algo.lower() == 'salsa20':
-                pre = 'salsa20.' if import_mode == 'path' else ''
-                self.__crypto_obj = eval(f'{pre}salsa20(self.password)')
+                self.__crypto_obj = salsa20.salsa20(self.password)
 
     def get_key(self) -> Optional[bytes]:
         try:
